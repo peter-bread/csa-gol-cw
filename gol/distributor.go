18bc456a7a94e4c298d5240bd7abb3e9af2aadce
Peter Sheehan <ak22112@bristol.ac.uk>
2023-10-31 00:46:53 +0000

    all tests pass for single-thread

--- gol/distributor.go
+++ gol/distributor.go
@@ -2,6 +2,7 @@ package gol
 
 import (
 	"fmt"
+
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -16,9 +17,12 @@ type distributorChannels struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-	filename := fmt.Sprintf("%vx%v.pgm", p.ImageWidth, p.ImageHeight)
-	c.ioFilename <- filename
+	filename := fmt.Sprintf("%vx%v", p.ImageWidth, p.ImageHeight)
+	fmt.Println("filename stored")
 	c.ioCommand <- ioInput
+	fmt.Println("command into chan")
+	c.ioFilename <- filename
+	fmt.Println("filename into chan")
 
 	// TODO: Create a 2D slice to store the world.
 	world := make([][]byte, p.ImageHeight)
